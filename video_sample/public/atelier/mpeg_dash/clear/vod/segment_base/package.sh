dbf8724a3240ff790c4cfdd08dd828c35daec2df
Kazuki Nagai <platong8249@gmail.com>
2024-03-18 18:53:18 +0900

    Add segment base clear

---
+++ video_sample/public/atelier/mpeg_dash/clear/vod/segment_base/package.sh
@@ -0,0 +1,4 @@
+packager \
+  in=../../../../../blender/progressive_mp4/big_buck_bunny_720p_surround.mp4,stream=audio,output=audio.mp4 \
+  in=../../../../../blender/progressive_mp4/big_buck_bunny_720p_surround.mp4,stream=video,output=video.mp4 \
+  --mpd_output manifest.mpd \
