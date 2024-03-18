324beaa7e488f5ccd7eb2c7004d66b92eabc75bb
Kazuki Nagai <platong8249@gmail.com>
2024-03-18 18:29:05 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-wget -nc https://ia600300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4 > video_sample/public/blender/progressive_mp4
+mkdir -p video_sample/public/blender/progressive_mp4
+wget -nc -P video_sample/public/blender/progressive_mp4 https://ia600300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4
